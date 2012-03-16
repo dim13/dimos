068a3ce53eae4be94377e3e57cdc8a6b4e3c2916
Dimitri Sokolyuk <demon@dim13.org>
2012-03-16 14:56:48 +0000

    don't set default values

--- firmware/firmware.c
+++ firmware/firmware.c
@@ -15,6 +15,10 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
+#ifndef BAUD
+#define BAUD 9600
+#endif
+
 #include <avr/io.h>
 #include <avr/boot.h>
 #include <avr/wdt.h>
