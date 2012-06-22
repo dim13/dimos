61b27cc4a2ecde45e1e1c54b9249718cb7b563de
demon <demon>
2012-06-22 11:16:25 +0000

    adjust number of run queues

--- kernel/kernel.h
+++ kernel/kernel.h
@@ -69,7 +69,7 @@
 #define MSEC(T)	((uint32_t)(T) * kHz)
 #define SEC(T)	((uint32_t)(T) * Hz)
 
-enum Prio { High, Mid, Low, Idle, nPrio };
+enum Prio { High, Low = 3, Idle, nPrio };
 enum SysRQ { nTask, Prio };
 
 /* __BEGIN_DECLS */
